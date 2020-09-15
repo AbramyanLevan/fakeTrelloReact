5d31e0d83a5165ac04569a737084b7f3c91d4232
AbramyanLevan <levabr32@gmail.com>
2020-09-15 21:07:53 +0300

    create the layout

---
+++ src/Components/Content.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../index.css'
+
+const Content = () => {
+    return (
+        <div className='content'>
+            
+        </div>
+    )
+}
+
+export default Content
